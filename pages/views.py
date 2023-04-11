dbb57620080a505fc5c9032f48b73a4cb505c79b
Alex Howard <alex-m-howard@pm.me>
2023-04-10 22:00:19 -0400

    Added user auth routes

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
