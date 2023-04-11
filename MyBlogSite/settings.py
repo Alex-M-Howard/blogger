cd2c65a14612bc6a59b226a7b9ca6ffd0f2c5b5f
Alex Howard <alex-m-howard@pm.me>
2023-04-10 21:57:21 -0400

    Added redirect urls

--- MyBlogSite/settings.py
+++ MyBlogSite/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login and Logout Redirects
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+SIGNUP_REDIRECT_URL = '/'
