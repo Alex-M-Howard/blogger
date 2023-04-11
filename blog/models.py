e4e99b55bdc1f6d15a20159306c7ae334a0b561b
Alex Howard <alex-m-howard@pm.me>
2023-04-11 05:32:13 -0400

    Created Blog Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+    comments = models.ManyToManyField('Comment')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} said {self.body}"
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'body')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
